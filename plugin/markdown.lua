9b44e15b0d48982220f08363f2199fb39246694c
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-20 17:35:55 +0200

    feat(markdown): implement modular markdown plugin loading

--- plugin/markdown.lua
+++ plugin/markdown.lua
@@ -1,34 +1,6 @@
-local filetypes = {
+vim.g.markdown_plugin = "markview"
+vim.g.markdown_filetypes = {
 	"markdown",
 }
 
-local plugins = {
-	markview = {
-		vim.pack.add({ "https://github.com/OXY2DEV/markview.nvim" }, { load = true, confirm = false }),
-		require("markview").setup({
-			ft = filetypes,
-			opts = {
-				preview = {
-					filetypes = filetypes,
-					ignore_buftypes = {},
-				},
-			},
-		}),
-	},
-
-	render_markdown = {
-		vim.pack.add(
-			{ "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
-			{ load = true, confirm = false }
-		),
-		require("render-markdown").setup({
-			ft = filetypes,
-			opts = {
-				anti_conceal = { enabled = false },
-				file_types = filetypes,
-			},
-		}),
-	},
-}
-
-return plugins.render_markdown
+return { require("plugins.markdown." .. vim.g.markdown_plugin) }
