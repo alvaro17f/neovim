46339e0317deed1d4058269f17cf184d2c0c10f3
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-22 09:46:53 +0200

    feat: initialize todo-comments plugin

--- plugin/todo-comments.lua
+++ plugin/todo-comments.lua
@@ -3,6 +3,8 @@ vim.pack.add({
   "https://github.com/folke/todo-comments.nvim",
 }, { load = true, confirm = false })
 
+require("todo-comments").setup()
+
 vim.keymap.set("n", "<leader>ft", function()
   Snacks.picker.todo_comments()
 end, { desc = "show TODO" })
