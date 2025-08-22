c4dc7aff7faa2eecf53dfe22df2b3213e2e4a1dc
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-22 17:38:27 +0200

    fix: npm-info

--- plugin/npm-info.lua
+++ plugin/npm-info.lua
@@ -1,7 +1,7 @@
 vim.pack.add({ "https://github.com/pxnditxyr/npm-info.nvim" }, { load = true, confirm = false })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "package.json",
+vim.api.nvim_create_autocmd("BufReadPre", {
+  pattern = { "package.json" },
   callback = function()
     require("npm-info").setup()
   end,
