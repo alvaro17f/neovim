512d35479bd57d1977ebedfca3844b110920fbb8
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-22 17:49:17 +0200

    fix: snacks

--- lua/plugins/snacks/dim.lua
+++ lua/plugins/snacks/dim.lua
@@ -4,10 +4,4 @@ M.config = {
   dim = { enabled = true },
 }
 
-M.keymaps = {
-  vim.keymap.set("n", "<leader>d", function()
-    require("snacks").dim()
-  end, { desc = "Toggle Dim" }),
-}
-
 return M
