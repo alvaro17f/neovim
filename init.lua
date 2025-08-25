312396f281ccbca9a65b330b40b0cf5e188118e6
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-25 11:59:30 +0200

    feat: remove incline.nvim plugin in favor of native winbar solution

--- init.lua
+++ init.lua
@@ -1,12 +1,4 @@
-require("vim._extui").enable({
-  enable = true, -- Whether to enable or disable the UI.
-  msg = {
-    ---@type 'cmd'|'msg'
-    target = "msg",
-    timeout = 4000,
-  },
-})
-
+require("core.ui")
 require("core.flags")
 require("core.autocmd")
 require("core.options")
---
+++ lua/core/ui.lua
@@ -0,0 +1,28 @@
+------------------------------------
+-- UI
+------------------------------------
+require("vim._extui").enable({
+  enable = true,
+  msg = {
+    target = "msg",
+    timeout = 4000,
+  },
+})
+
+------------------------------------
+-- WINBAR
+------------------------------------
+vim.api.nvim_create_autocmd("WinResized", {
+  callback = function(args)
+    if
+      not vim.api.nvim_win_get_config(0).zindex -- Not a floating window
+      and vim.bo[args.buf].buftype == "" -- Normal buffer
+      and vim.api.nvim_buf_get_name(args.buf) ~= "" -- Has a file name
+      and not vim.wo[0].diff -- Not in diff mode then
+    then
+      vim.o.winbar = "%=" .. "%{%v:lua.require('utils.winbar').winbar_get_icon()%}" .. "  " .. "%f" .. " " .. "%m"
+    else
+      vim.o.winbar = nil
+    end
+  end,
+})
